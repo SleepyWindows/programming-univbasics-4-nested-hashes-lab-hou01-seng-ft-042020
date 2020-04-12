32454da30bc0a240201991a3ad205479004f4794
edwin calvillo <calvillo.edwin@gmail.com>
2020-04-12 01:26:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,8 +89,9 @@ def adding_matz
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
   }
+  programmer_hash
 end
-programmer_hash
+
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable.
